3c423891631a7ff2ee4bee721d11f1f00be0163d
mariabeatrizaparecidadasilva <168091474+mariabeatrizaparecidadasilva@users.noreply.github.com>
2025-09-19 16:01:45 -0300

    Arrumei o html de senha e coloquei um limite para o gerador de senha(64)

--- scripts/senhas.py
+++ scripts/senhas.py
@@ -1,5 +1,11 @@
 import random, string
 
 def gerar_senha(tamanho=12):
+    # limites de segurança
+    if tamanho < 4:
+        tamanho = 4
+    elif tamanho > 64:
+        tamanho = 64
+
     caracteres = string.ascii_letters + string.digits + "!@#$%&*?"
     return "".join(random.choice(caracteres) for _ in range(tamanho))
