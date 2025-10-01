736d6631067672b6244bde1808798d2a01408d19
mariabeatrizaparecidadasilva <168091474+mariabeatrizaparecidadasilva@users.noreply.github.com>
2025-10-01 10:45:53 -0300

    atualizei o app.py para teste de environ

--- app.py
+++ app.py
@@ -444,4 +444,5 @@ def arquivos(filename):
 # Inicialização
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta do Render ou 5000 localmente
+    app.run(host="0.0.0.0", port=port, debug=True)
